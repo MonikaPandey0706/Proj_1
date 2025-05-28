c6df1230b8585074420f1402589814b06f92e622
MonikaPandey0706 <monika.pandey4@s.amity.edu>
2025-05-28 18:33:57 +0530

    c

--- python.py
+++ python.py
@@ -1,10 +1,11 @@
 #Code to detech if the candidate is allowed to vote or not
 #LANGUAGE: python
+#tring to do git merge
 
 
 
 Age = int(input("Enter your age to check if ur eligible for voting: "))
-if age >= 18:
+if Age >= 18:
     print("You are an adult and eligible to vote")
 else:
     print("You are a minor.")
