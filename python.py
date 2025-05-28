e57c6c41e5ab6d558a82cb692b1e70478b32ff3b
MonikaPandey0706 <monika.pandey4@s.amity.edu>
2025-05-28 16:08:29 +0530

    Added new comments

--- python.py
+++ python.py
@@ -1,3 +1,7 @@
+#Code to detech if the candidate is allowed to vote or not
+#LANGUAGE: python 
+
+
 age = int(input("Enter your age: "))
 if age >= 18:
     print("You are an adult and eligible to vote")
